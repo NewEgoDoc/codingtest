90f05060a8f906e45c4ffdc47a6d4710d42d44ea
Juhan <juhan211@naver.com>
2022-01-04 22:35:02 +0900

    다음큰숫자 첫번째 정답 뭔가 더 리팩토링과 클린코드의 중요성이 필요한것 같다

--- src/main/java/coding/test/programmers/Lv2/Lv2다음큰숫자.java
+++ src/main/java/coding/test/programmers/Lv2/Lv2다음큰숫자.java
@@ -1,15 +1,38 @@
 package coding.test.programmers.Lv2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Lv2다음큰숫자 {
     public int solution(int n) {
         int answer = 0;
-        return answer;
+
+        int oneCount = countBinary(n);
+
+        for (int i = n+1; i > n; i++){
+            if(oneCount == countBinary(i)){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    private int countBinary(int n) {
+        int count = 0;
+        String binaryNumber = Integer.toBinaryString(n);
+        for(char num: binaryNumber.toCharArray()){
+            if(num == '1'){
+                count++;
+            }
+        }
+        return count;
     }
 
     @Test
     public void test(){
-        System.out.println(12);
+        Assertions.assertEquals(solution(78),83);
+        Assertions.assertEquals(solution(15),23);
+        //78 83 15 23
     }
 }
