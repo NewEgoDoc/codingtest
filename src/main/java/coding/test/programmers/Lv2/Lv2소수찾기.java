b7b01ce3bec2c65b95f9bf973b488b83d7159fdc
Juhan <juhan211@naver.com>
2022-01-08 23:21:46 +0900

    문제풀기 준비 저장

---
+++ src/main/java/coding/test/programmers/Lv2/Lv2소수찾기.java
@@ -0,0 +1,19 @@
+package coding.test.programmers.Lv2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Lv2소수찾기 {
+
+    public int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+
+    @Test
+    public void test(){
+        assertEquals(solution("asdf"),0);
+    }
+}
---
+++ src/main/java/coding/test/programmers/Lv2/Lv2스킬트리.java
@@ -0,0 +1,19 @@
+package coding.test.programmers.Lv2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Lv2스킬트리 {
+
+    public int solution(String skill, String[] skill_trees) {
+        int answer = -1;
+        return answer;
+    }
+
+    @Test
+    public void test(){
+        assertEquals(solution("sdfsdf",new String[]{"","",""}),1);
+    }
+}
