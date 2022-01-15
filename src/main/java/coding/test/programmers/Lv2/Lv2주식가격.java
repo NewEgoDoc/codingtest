410f0660adddb8d72c29b434dd67adee08e0fb81
Juhan <juhan211@naver.com>
2022-01-15 18:25:08 +0900

    현재에 있는 포인트를 설정하고 그 아래에 대한 내용을 정리해서 풀면 된다

--- src/main/java/coding/test/programmers/Lv2/Lv2주식가격.java
+++ src/main/java/coding/test/programmers/Lv2/Lv2주식가격.java
@@ -9,26 +9,31 @@ import java.util.List;
 public class Lv2주식가격 {
     public int[] solution(int[] prices) {
         List<Integer> list = new ArrayList<>();
-        int second;
-        for (int i = 0; i < prices.length-1; i++) {
-            second = 0;
+
+        for (int i = 0; i < prices.length -1; i++) {
+            int presentPoint = prices[i];
+            int period = 0;
             for (int j = i+1; j < prices.length; j++) {
-                if(prices[i] <= prices[j]){
-                    second++;
+                period++;
+                if(prices[j] < presentPoint){
+                    break;
                 }
             }
-            list.add(second);
+            list.add(period);
         }
-
         list.add(0);
 
-        int[] answer = new int[list.size()];
-
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
+        int[] answer = toIntArray(list);
 
         return answer;
+    }
+
+    private int[] toIntArray(List<Integer> list) {
+        int[] array = new int[list.size()];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = list.get(i);
+        }
+        return array;
     }
 
     @Test
