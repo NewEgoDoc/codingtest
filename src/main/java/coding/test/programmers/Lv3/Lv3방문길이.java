bba6f2a86055d5d9e3ddbb21ed73cf5368bd4422
Juhan <juhan211@naver.com>
2022-01-27 02:03:12 +0900

    Set의 활용

---
+++ src/main/java/coding/test/programmers/Lv3/Lv3방문길이.java
@@ -0,0 +1,71 @@
+package coding.test.programmers.Lv3;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+public class Lv3방문길이 {
+    public int solution(String dirs){
+        HashSet<String> hs = new HashSet<>();
+        int nowX = 0;
+        int nowY = 0;
+
+        StringBuilder sb = null;
+
+        for (int i = 0; i < dirs.length(); i++) {
+            int nextX = nowX;
+            int nextY = nowY;
+            sb = new StringBuilder();
+
+            if(dirs.charAt(i) == 'U'){
+                nextY++;
+                sb.append(nowX);
+                sb.append(nowY);
+                sb.append(nextX);
+                sb.append(nextY);
+            }
+
+            if(dirs.charAt(i) == 'D'){
+                nextY--;
+                sb.append(nextX);
+                sb.append(nextY);
+                sb.append(nowX);
+                sb.append(nowY);
+            }
+
+            if(dirs.charAt(i) == 'R'){
+                nextX++;
+                sb.append(nowX);
+                sb.append(nowY);
+                sb.append(nextX);
+                sb.append(nextY);
+            }
+
+            if(dirs.charAt(i) == 'L'){
+                nextX--;
+                sb.append(nextX);
+                sb.append(nextY);
+                sb.append(nowX);
+                sb.append(nowY);
+            }
+
+            if(nextX < -5 || nextY < -5 || nextX > 5 || nextY > 5){
+                continue;
+            }
+
+            hs.add(sb.toString());
+            nowX = nextX;
+            nowY = nextY;
+        }
+
+        return hs.size();
+    }
+
+    @Test
+    public void test(){
+        Assertions.assertEquals(solution("ULURRDLLU"),7);
+        Assertions.assertEquals(solution("LULLLLLLU"),7);
+        Assertions.assertEquals(solution("LLLLLLLUUUUUUUUUUURRRRRRRRRRRRRRRRRRRRRRRR"),20);
+    }
+}
