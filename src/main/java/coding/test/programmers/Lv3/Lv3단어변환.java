080d4483ab7e6699175e66fc47e8fe07fcb2ed0f
Juhan <juhan211@naver.com>
2022-01-29 01:20:27 +0900

    단어변환 dfs의 활용 복습 완료

--- src/main/java/coding/test/programmers/Lv3/Lv3단어변환.java
+++ src/main/java/coding/test/programmers/Lv3/Lv3단어변환.java
@@ -9,8 +9,39 @@ import java.util.Collections;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lv3단어변환 {
-
     int min;
+    public int solution(String begin, String target, String[] words) {
+        min = 51;
+        boolean visited[] = new boolean[words.length];
+        dfs(begin, target, words,0,visited);
+        return min == 51? 0:min;
+    }
+
+    private void dfs(String word, String target, String[] words, int index, boolean[] visited) {
+        if(word.equals(target)){
+            System.out.println("word = " + word);
+            System.out.println("target = " + target);
+            System.out.println("index = " + index);
+            if(index < min) min = index;
+        }
+        for (int i = 0; i < words.length; i++) {
+            if(!visited[i] && isOneDifferent(word, words[i])){
+                visited[i] = true;
+                dfs(words[i],target,words,index + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
+
+    private boolean isOneDifferent(String word, String another) {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) != another.charAt(i)) count++;
+        }
+        return (count < 2);
+    }
+
+    /*int min;
 
     public int solution(String begin, String target, String[] words) {
         min = 51;
@@ -38,17 +69,15 @@ public class Lv3단어변환 {
 
     private boolean check(String word, String nextWord) {
         int count = 0;
-
         for (int i = 0; i < word.length(); i++) {
             if(word.charAt(i) != nextWord.charAt(i)) count++;
         }
-
         return (count <= 1);
     }
-
+    */
     @Test
     public void test(){
-        //assertEquals(solution("hit","cog",new String[]{"hot", "dot", "dog", "lot", "log", "cog"}),4);
+        //(solution("hit","cog",new String[]{"hot", "dot", "dog", "lot", "log", "cog"}),4);
         assertEquals(solution("hit","cog",new String[]{"hot", "dot", "dog", "lot", "log"}),0);
     }
 
