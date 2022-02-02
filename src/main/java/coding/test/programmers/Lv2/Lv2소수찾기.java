8b46302b8827555dd02e6bbe2e4d61ab048d4593
Juhan <juhan211@naver.com>
2022-02-03 01:08:50 +0900

    뭔가 복잡한 방법으로 소수찾기를 완성... 조금 다듬고 간결한 방법을 찾아봐야겠다

--- src/main/java/coding/test/programmers/Lv2/Lv2소수찾기.java
+++ src/main/java/coding/test/programmers/Lv2/Lv2소수찾기.java
@@ -2,54 +2,52 @@ package coding.test.programmers.Lv2;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lv2소수찾기 {
-    StringBuilder sb = null;
-    int n = 0;
-    boolean[] visited = null;
-    String numbersCopy;
-    List<Integer> numberList = new ArrayList<>();
+    StringBuilder sb = new StringBuilder();
     public int solution(String numbers) {
-        numbersCopy = new StringBuilder().append(numbers).toString();
-        sb = new StringBuilder();
-        n = numbers.length();
-        visited = new boolean[n];
 
-        int answer = 0;
-        int[] numberArray = makePossibleNumbers();
-        return answer;
-    }
-
-    private int[] makePossibleNumbers() {
-        int[] array;
-
-        for(int i = 0 ;i < n; i++){
-            permutation(0,i);
+        boolean[] visited = new boolean[numbers.length()];
+        Set<Integer> possibleNums = new HashSet<>();
+        for (int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, visited, 0, i+1, possibleNums);
         }
 
-        System.out.println(numberList);
+        System.out.println("possibleNums = " + possibleNums);
 
-        return new int[0];
+        return possibleNums.size();
     }
 
-
-    private void permutation(int index, int x) {
-        if(index == x){
-            numberList.add(Integer.parseInt(sb.toString()));
-        }
-
-        for (int i = 0; i < n; i++) {
-            if(visited[i] == false){//방문 한적이 없다면
-                visited[i] = true;//방문체크
-                sb.append(numbersCopy.charAt(i));
-                permutation(index+1,x);//인덱스만 하나로 앞으로 해주고 모두 방문해본다.
-                visited[i] = false;//방문하고 나온후에는 해당 지점은 방문 취소 처리
+    private void dfs(String numbers, boolean[] visited, int index, int size, Set<Integer> possibleNums) {
+        if(index == size){
+            if(isPrime(Integer.parseInt(sb.toString()))) {
+                possibleNums.add(Integer.parseInt(sb.toString()));
             }
         }
+
+        for (int i = 0; i < numbers.length(); i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                sb.append(numbers.charAt(i));
+                dfs(numbers,visited,index+1,size,possibleNums);
+                sb.deleteCharAt(sb.length()-1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    private boolean isPrime(int num){
+        if(num == 0 || num == 1) return false;
+
+        for (int i = 2; i < num; i++) {
+            if(num%i == 0) return false;
+        }
+        return true;
     }
 
     @Test
