e2b421c4d3f592bc37e4457fa79955b12881c939
Juhan <juhan211@naver.com>
2022-02-10 00:21:38 +0900

    완전 탐색이 아닌 dp를 통해서 이전에 연산 값을 저장해나가며 풀어야 한다

--- src/main/java/coding/test/programmers/Lv3/Lv3정수삼각형.java
+++ src/main/java/coding/test/programmers/Lv3/Lv3정수삼각형.java
@@ -1,18 +1,42 @@
 package coding.test.programmers.Lv3;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Lv3정수삼각형 {
+    int max = 0;
     public int solution(int[][] triangle) {
-        int answer = 0;
-        return answer;
+        max = 0;
+
+        dfs(triangle,0,0,0);
+
+        System.out.println("max = " + max);
+        return max;
+    }
+
+    private void dfs(int[][] triangle, int depth, int index, int sum) {
+        if(depth == triangle.length-1){
+            if(max < sum) {
+                max = sum;
+            }
+            return;
+        }
+        System.out.println("sum = " + sum);
+
+
+        if(index - 1 >= 0)
+            dfs(triangle,depth+1,index-1,sum+triangle[depth][index]);
+        dfs(triangle,depth+1,index,sum+triangle[depth][index]);
+        dfs(triangle,depth+1,index+1,sum+triangle[depth][index]);
     }
 
     @Test
     public void test(){
-        assertEquals(solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}),30);
+        assertEquals(solution(new int[][]{{7},
+                                         {3,8},
+                                        {8,1,0},
+                                       {2,7,4,4},
+                                      {4,5,2,6,5}}),30);
     }
 }
