b9cb53ba3cb4da77f926c85c843b126c47271d78
Juhan <juhan211@naver.com>
2022-02-28 22:36:29 +0900

    재귀함수 세가지 return 재귀 재귀 앞 재귀 뒤

---
+++ src/main/java/coding/test/summary/important/DFS.java
@@ -0,0 +1,41 @@
+package coding.test.summary.important;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class DFS {
+
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        visited = new boolean[input + 1];
+        dfs(1,input);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void dfs(int index, int last) {
+        if(index >= last+1){
+            for (int i = 1; i <= last; i++) {
+                if(visited[i]) System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        visited[index] = true;
+        dfs(index+1,last);
+
+        visited[index] = false;
+        dfs(index+1,last);
+    }
+}
