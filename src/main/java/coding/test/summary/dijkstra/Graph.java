79570a250533c85dad3c7ecc6fb08a422406cdae
Juhan <juhan211@naver.com>
2022-03-14 02:11:10 +0900

    다익스트라 개념을 개인적으로 다시 정리

--- src/main/java/coding/test/summary/dijkstra/Graph.java
+++ src/main/java/coding/test/summary/dijkstra/Graph.java
@@ -1,5 +1,8 @@
 package coding.test.summary.dijkstra;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -14,18 +17,27 @@ public class Graph{
            this.index = index;
            this.cost = cost;
        }
+
+       @Override
+       public String toString() {
+           return "Node{" +
+                   "index=" + index +
+                   ", cost=" + cost +
+                   '}';
+       }
    }
 
-   public int solution(int V, int E){
+   public int solution(int V, int E,int target){
 
-       int start = 0;
-       int[] distance = new int[V+1];
+       int start = 1;
+       int[] distance = new int[E+1];
        for (int i = 0; i < distance.length; i++) {
            distance[i] = Integer.MAX_VALUE;
        }
+       boolean[] visited = new boolean[E+1]; visited[0] = true;
 
        List<List<Node>> graph = new ArrayList<>();
-       for (int i = 0; i < V+1; i++) {
+       for (int i = 0; i < E+1; i++) {
            graph.add(new ArrayList<>());
        }
        graph.get(1).add(new Node(2,2));
@@ -36,31 +48,63 @@ public class Graph{
        graph.get(2).add(new Node(6,10));
        graph.get(2).add(new Node(1,2));
 
+       graph.get(3).add(new Node(1,5));
+       graph.get(3).add(new Node(2,7));
+       graph.get(3).add(new Node(4,1));
+       graph.get(3).add(new Node(5,2));
+       graph.get(3).add(new Node(6,5));
 
+       graph.get(4).add(new Node(3,1));
+       graph.get(4).add(new Node(5,7));
+       graph.get(4).add(new Node(1,3));
+
+       graph.get(5).add(new Node(3,2));
+       graph.get(5).add(new Node(4,7));
+       graph.get(5).add(new Node(6,2));
+
+       graph.get(6).add(new Node(2,10));
+       graph.get(6).add(new Node(3,5));
+       graph.get(6).add(new Node(5,2));
+
+       for (int i = 0; i < E+1; i++) {
+           System.out.println(i+":"+ graph.get(i));
+       }
+
+       dijkstra(start, distance, visited, graph);
+
+       for (int i = 0; i < distance.length; i++) {
+           System.out.print(distance[i] + " ");
+       }
+       System.out.println();
+
+        return distance[target];
+   }
+
+    private void dijkstra(int start, int[] distance, boolean[] visited, List<List<Node>> graph) {
         PriorityQueue<Node> pq = new PriorityQueue<>(
             (o1, o2)-> Integer.compare(o1.cost, o2.cost)
         );
 
         pq.offer(new Node(start, 0));
         distance[start]= 0;
+
         while(!pq.isEmpty()){
             Node currentNode = pq.poll();
-            if (distance[currentNode.index] < currentNode.cost) {
-                continue;
-            }
+            visited[currentNode.index] = true;
 
             for (int i = 0; i < graph.get(currentNode.index).size(); i++) {
                 Node nextNode = graph.get(currentNode.index).get(i);
-                if(distance[nextNode.index] > currentNode.cost+ nextNode.cost){
+                if(distance[nextNode.index] > currentNode.cost + nextNode.cost){
                     distance[nextNode.index] = currentNode.cost+ nextNode.cost;
                     pq.offer(new Node(nextNode.index, distance[nextNode.index]));
                 }
             }
-
         }
+    }
 
-        return 0;
+    @Test
+   public void test(){
+       Assertions.assertEquals(solution(5,6,6),8);
    }
-
 
 }
