df416302df17c2bc9744d3b04bea5fae5c127746
Juhan <juhan211@naver.com>
2022-09-22 14:40:55 +0900

    모의 테스트 진행 1st 이전 방식이 아니라 시작점으로 부터 모든 곳의 다익스트라를 써서 비용을 구하고 각 지점으로부터의 비용을 구하여 더하는 방식!

--- src/main/java/coding/test/trial/no1/Fourth.java
+++ src/main/java/coding/test/trial/no1/Fourth.java
@@ -1,57 +1,82 @@
 package coding.test.trial.no1;
 
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.PriorityQueue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Fourth {
-
 	int[][] matrix;
 	int N;
 	public int solution(int n, int s, int a, int b, int[][] fares) {
 		N = n;
-		matrix = new int[N+1][N+1];
+		matrix = new int[N][N];
 
 		for (int i = 0; i < fares.length; i++) {
 			int x = fares[i][0];
 			int y = fares[i][1];
 			int cost = fares[i][2];
-			matrix[x][y] = cost;
-			matrix[y][x] = cost;
+			matrix[x-1][y-1] = cost;
+			matrix[y-1][x-1] = cost;
 		}
 
-		int[] distanceFromS = dijkstra(s);
-		int[] distanceFromB = dijkstra(b);
-		int[] distanceFromA = dijkstra(a);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print("\t" + matrix[i][j]);
+			}
+			System.out.println();
+		}
 
-		int sab = distanceFromS[a] + distanceFromA[b];
-		int sba = distanceFromS[b] + distanceFromB[a];
+		int[] distanceFromS = dijkstra(s-1);
 
-		return Math.min(sab, sba);
+		int minDistance = Integer.MAX_VALUE;
+		for (int i = 0; i < N; i++) {
+			int[] distances = dijkstra(i);
+			int	distance = distanceFromS[i] + distances[a-1] + distances[b-1];
+			if(distance < minDistance) {
+				minDistance = distance;
+			}
+		}
+		return minDistance;
 	}
 
 	private int[] dijkstra(int start) {
+		int[] distance = new int[N];
+		Arrays.fill(distance, Integer.MAX_VALUE);
 		PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-		boolean visited[] = new boolean[N + 1];
-		visited[0] = true;
+		distance[start] = 0;
+		boolean visited[] = new boolean[N];
 		
 		pq.offer(new int[]{0,start});
-		visited[start] = true;
-		// while (!pq.isEmpty()){
-		//
-		// }
 
+		while (!pq.isEmpty()){
+			int[] cur = pq.poll();
+			int index = cur[1];
 
-		return null;
+			if(visited[index]) continue;
+
+			visited[index] = true;
+
+			for (int i = 0; i < N; i++) {
+				if(matrix[index][i] == 0){
+					continue;
+				}
+				if(distance[index] + matrix[index][i] < distance[i]){
+					distance[i] = distance[index] + matrix[index][i];
+					pq.add(new int[]{distance[i], i});
+				}
+			}
+		}
+		return distance;
 	}
 
 	@Test
 	void test(){
-		Map<String, Integer> courseCount = new HashMap<>();
-		final Integer a = courseCount.get('a');
-		System.out.println("a = " + a);
+		Assertions.assertEquals(solution(6,4,6,2,new int[][]{{4, 1, 10}, {3, 5, 24}, {5, 6, 2}, {3, 1, 41}, {5, 1, 24}, {4, 6, 50}, {2, 4, 66}, {2, 3, 22}, {1, 6, 25}}),82);
+		Assertions.assertEquals(solution(7,3,4,1,new int[][]{{5, 7, 9}, {4, 6, 4}, {3, 6, 1}, {3, 2, 3}, {2, 1, 6}}), 14);
+		Assertions.assertEquals(solution(6,4,5,6,new int[][]{{2,6,6},{6,3,7},{4,6,7},{6,5,11},{2,5,12},{5,3,20},{2,4,8},{4,3,9}}),18);
+
 	}
 }
