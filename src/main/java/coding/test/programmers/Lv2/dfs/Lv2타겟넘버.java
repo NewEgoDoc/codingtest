9201faa0e16ed48818b903554697265b2a23e587
Juhan <juhan211@naver.com>
2022-09-26 20:11:15 +0900

    dfs중에 stack으로 구현!

--- src/main/java/coding/test/programmers/Lv2/dfs/Lv2타겟넘버.java
+++ src/main/java/coding/test/programmers/Lv2/dfs/Lv2타겟넘버.java
@@ -2,12 +2,17 @@ package coding.test.programmers.Lv2.dfs;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 import org.junit.jupiter.api.Test;
 
 public class Lv2타겟넘버 {
 
 	public int solution(int[] numbers, int target) {
-		return dfs(numbers,0,0,target);
+		//return dfs(numbers,0,0,target);
+		return dfs(numbers,target);
 	}
 
 	public int dfs(int[] numbers, int depth, int sum, int target){
@@ -20,6 +25,37 @@ public class Lv2타겟넘버 {
 			return dfs(numbers,depth+1,sum+numbers[depth],target)
 				+ dfs(numbers,depth+1,sum-numbers[depth],target);
 		}
+	}
+
+	public int dfs(int[] numbers, int target){
+		int count = 0;
+		Stack<Integer> stack = new Stack<>();
+		List<Integer> list;
+
+		stack.push(numbers[0]);
+		stack.push(numbers[0]*-1);
+
+		for (int i = 1; i < numbers.length; i++) {
+			int nextNum = numbers[i];
+			list = new ArrayList<Integer>();
+
+			while(!stack.isEmpty()) {
+				int stackNum = stack.pop();
+
+				list.add(stackNum+nextNum);
+				list.add(stackNum+(nextNum*-1));
+			}
+
+			for (int j = 0; j < list.size(); j++) {
+				stack.push(list.get(j));
+			}
+		}
+
+		for (Integer sum : stack) {
+			if(sum == target)
+				count++;
+		}
+		return count;
 	}
 
 
