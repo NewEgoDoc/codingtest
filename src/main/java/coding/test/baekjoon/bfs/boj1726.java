b380b15878e432a3ee4c8028cb574e0255986381
Juhan <juhan211@naver.com>
2022-10-12 18:20:42 +0900

    도저히 안되겠어서 main으로 푸는 방법을 선택했다 현재 제출까지 마치고 성공!

--- src/main/java/coding/test/baekjoon/bfs/boj1726.java
+++ src/main/java/coding/test/baekjoon/bfs/boj1726.java
@@ -1,58 +1,143 @@
 package coding.test.baekjoon.bfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class boj1726 {
 
-	int[] dx = {0, -1, 1, 0, 0};
-	int[] dy = {0, 0, 0, -1, 1};
+	static int[] dx = { 0, 0, 1, -1 }; // 동서남북
+	static int[] dy = { 1, -1, 0, 0 };
 
-	boolean[][] visited;
+	static int n, m;
+	static int sx, sy, sd;
+	static int ex, ey, ed;
 
-	int[][] maze;
+	static int[][] map;
+	static int[][][] visited;
+	static Queue<Node> q = new LinkedList<>();
 
-	int solution(int[][] map, int[] start, int[] end){
-		maze = map;
-		visited = new boolean[map.length][map[0].length];
-		bfs(new int[]{start[0] - 1, start[1] - 1, start[2] },new int[]{ end[0] - 1, end[1] - 1, end[2]});
+	static class Node {
+		int x, y, dir, cost;
 
-		return 0;
+		Node(int x, int y, int dir, int cost) {
+			this.x = x;
+			this.y = y;
+			this.dir = dir;
+			this.cost = cost;
+		}
 	}
 
-	private void bfs(int[] start, int[] end) {
-		Queue<int[]> q = new LinkedList<>();
-		q.offer(start);
+	static void print() {
+		for(int i = 0 ; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				for(int k = 0; k < 4; k++) {
+					System.out.print(visited[i][j][k]+" ");
+				}
+				System.out.print("\t");
+			}
+			System.out.println("\n");
+		}
+	}
 
-		while(!q.isEmpty()){
-			int[] poll = q.poll();
+	static boolean isRange(int x, int y) {
+		if (x >= 0 && x < n && y >= 0 && y < m)
+			return true;
+		return false;
+	}
 
-			for (int i = 1; i <= 4; i++) {
-				int nx = poll[0] + dx[i];
-				int ny = poll[1] + dy[i];
+	static void bfs() {
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
 
-				if(nx < 0 || ny < 0 || nx >= maze.length || ny >= maze[0].length) continue;
+			for (int k = 1; k <= 3; k++) { // GO k - 1, 2 또는 3 현재 방향으로 전진
+				int kx = cur.x + (dx[cur.dir] * k);
+				int ky = cur.y + (dy[cur.dir] * k);
 
-
+				if (isRange(kx, ky) && visited[kx][ky][cur.dir] == -1) {
+					if (map[kx][ky] == 1)
+						break;
+					visited[kx][ky][cur.dir] = cur.cost+1;
+					q.add(new Node(kx, ky, cur.dir, cur.cost+1));
+				}
 			}
 
-		}
+			int dir = cur.dir;
 
+			switch (dir) { // 좌회전
+				case (0):dir=2;
+					break;
+				case (1):dir=3;
+					break;
+				case (2):dir=1;
+					break;
+				case (3):dir=0;
+					break;
+			}
+
+			if(visited[cur.x][cur.y][dir] == -1) {
+				visited[cur.x][cur.y][dir] = cur.cost+1;
+				q.add(new Node(cur.x, cur.y, dir, cur.cost+1));
+			}
+
+			dir = cur.dir;
+			switch (dir) { // 우회전
+				case (0):dir=3;
+					break;
+				case (1):dir=2;
+					break;
+				case (2):dir=0;
+					break;
+				case (3):dir=1;
+					break;
+			}
+
+			if(visited[cur.x][cur.y][dir] == -1) {
+				visited[cur.x][cur.y][dir] = cur.cost+1;
+				q.add(new Node(cur.x, cur.y, dir, cur.cost+1));
+			}
+		}
 	}
 
-	@Test
-	void test(){
-		Assertions.assertEquals(solution(
-			new int[][]{
-				{0, 0, 0, 0, 0, 0},
-				{0, 1, 1, 0, 1, 0},
-				{0, 1, 0, 0, 0, 0},
-				{0, 0, 1, 1, 1, 0},
-				{1, 0, 0, 0, 0, 0}}
-			,new int[] {4, 2, 3}
-			,new int[] {2, 4, 1}),9);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+
+		n = Integer.parseInt(stk.nextToken());
+		m = Integer.parseInt(stk.nextToken());
+		map = new int[n][m];
+		visited = new int[n][m][4];
+
+		for (int i = 0; i < n; i++) {
+			stk = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < m; j++) {
+				map[i][j] = Integer.parseInt(stk.nextToken());
+
+				for(int k = 0; k < 4; k++) {
+					visited[i][j][k] = -1;
+				}
+			}
+		}
+
+		stk = new StringTokenizer(br.readLine(), " ");
+		sx = Integer.parseInt(stk.nextToken()) - 1;
+		sy = Integer.parseInt(stk.nextToken()) - 1;
+		sd = Integer.parseInt(stk.nextToken()) - 1;
+		stk = new StringTokenizer(br.readLine(), " ");
+		ex = Integer.parseInt(stk.nextToken()) - 1;
+		ey = Integer.parseInt(stk.nextToken()) - 1;
+		ed = Integer.parseInt(stk.nextToken()) - 1;
+
+		visited[sx][sy][sd] = 0;
+		q.add(new Node(sx, sy, sd, 0));
+		bfs();
+
+		System.out.println(visited[ex][ey][ed]);
 	}
 }
