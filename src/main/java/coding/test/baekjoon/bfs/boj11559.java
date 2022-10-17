02d6fb99509498f3f20839aa1a8f9d6ec5deed98
Juhan <juhan211@naver.com>
2022-10-17 20:20:49 +0900

    dfs 통해 다이 맵을 끄집어 내리는 데에서 애먹었지만 단순하게 그냥 다 돌려버렸다

--- src/main/java/coding/test/baekjoon/bfs/boj11559.java
+++ src/main/java/coding/test/baekjoon/bfs/boj11559.java
@@ -1,7 +1,12 @@
 package coding.test.baekjoon.bfs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class boj11559 {
@@ -18,44 +23,105 @@ public class boj11559 {
 
 	char[][] matrix;
 
+
 	int solution(char[][] map){
 
+		count = 0;
 		matrix = map;
 		n = map.length;
 		r = map[0].length;
 
+		visited = new boolean[n][r];
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < r; j++) {
-
+				bfs(new int[]{i,j});
 			}
 		}
-		
-		dfs();
-		
-		return 0;
+
+		return count;
 	}
 
-	private void dfs(int[] point) {
-		
+	private void bfs(int[] point) {
+		if(matrix[point[0]][point[1]] == '.') {
+			visited[point[0]][point[1]] = true;
+			return;
+		}
+
+		Queue<int[]> q = new LinkedList<>();
+		List<int[]> track = new ArrayList<>();
+		q.add(point);
 		visited[point[0]][point[1]] = true;
+		track.add(point);
+		System.out.println("track = " + matrix[point[0]][point[1]]);
 
-		for (int i = 0; i < 4; i++) {
-			int nx = point[0] + dx[i];
-			int ny = point[1] + dy[i];
 
-			if(nx < 0 || ny < 0 || nx >= n || ny >= r) continue;
+		while(!q.isEmpty()){
 
-			if(matrix[nx][ny] == '.' || visited[nx][ny]) continue;
+			int[] poll = q.poll();
 
-			dfs(new int[]{nx, ny});
+			for (int i = 0; i < 4; i++) {
+				int nx = poll[0] + dx[i];
+				int ny = poll[1] + dy[i];
+				//System.out.println(nx + "," + ny);
 
+				if(nx < 0 || ny < 0 || nx >= n || ny >= r) continue;
+
+				if(matrix[point[0]][point[1]] != matrix[nx][ny] || visited[nx][ny]) continue;
+
+				track.add(new int[]{nx,ny});
+				q.add(new int[]{nx,ny});
+				visited[nx][ny] = true;
+			}
 
 		}
-		
+
+		if(track.size() >= 4){
+			pop(track);
+			bringDown();
+			visited = new boolean[n][r];
+		}
+
+	}
+
+	private void bringDown() {
+		for(int c = 0 ; c < r ; ++c) {
+			for(int sr = n-1 ; sr >= 0 ; --sr) {
+				if(matrix[sr][c] == '.') {
+					for(int nr = sr - 1 ; nr >= 0 ; --nr) {
+						if(matrix[nr][c] != '.') {
+							matrix[sr][c] = matrix[nr][c];
+							matrix[nr][c] = '.';
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private void pop(List<int[]> track) {
+		for (int[] point :
+			track) {
+			matrix[point[0]][point[1]] = '.';
+		}
+		count++;
 	}
 
 	@Test
 	void test(){
-		
+		Assertions.assertEquals(solution(new char[][]{
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','.','.','.','.','.'},
+				{'.','Y','.','.','.','.'},
+				{'.','Y','G','.','.','.'},
+				{'R','R','Y','G','.','.'},
+				{'R','R','Y','G','G','.'}
+		}),3);
 	}
 }
