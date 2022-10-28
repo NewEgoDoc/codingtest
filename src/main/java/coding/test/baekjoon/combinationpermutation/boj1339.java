1b2386a798e9775acf68a931233c1c143d74fae1
Juhan <juhan211@naver.com>
2022-10-28 20:30:46 +0900

    무작정 Permutation을 써서 푸는게 아니라 문제의 목적과 함께 쉽게 풀수있는 방법도 생각을 해보자. 아니 근데 이건 진짜 어떻게 생각해 낸거람;; 나온 값을 26개의 int형 배열을 생성하여 넣어준후, 정렬, 높은 값부터 9에서 0까지 곱하면서 더하면 된다 ㄷㄷ

--- src/main/java/coding/test/baekjoon/combinationpermutation/boj1339.java
+++ src/main/java/coding/test/baekjoon/combinationpermutation/boj1339.java
@@ -1,81 +1,37 @@
 package coding.test.baekjoon.combinationpermutation;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class boj1339 {
 
-	int n;
-	String[] strings;
-
-	String[] result;
-	int index;
-	int start;
-
-	int[] intReuslt;
-	int answer;
 	int solution(String[] words) {
-		n = words.length;
-		strings = words;
-		index = 0;
-		start = 0;
-		result = new String[n];
+		int [] alpha = new int[26];
+		int n = words.length;
 
-		combination();
+		//ex) AAA
+		for(int i = 0; i < n; i++){
+			int temp = (int)Math.pow(10,words[i].length()-1); //자릿수
 
-		return answer;
-	}
-
-	private void calculate() {
-		int sum = 0;
-		for (int i = 0; i < n; i++) {
-			
-			StringBuilder sb = new StringBuilder();
-			for (int j = 0; j < result[i].length(); j++) {
-				char word = change(result[i].charAt(j)); 
-				sb.append(word);
+			for(int j = 0; j < words[i].length(); j++){
+				alpha[(int)words[i].charAt(j) - 65] += temp;
+				temp /=10;
 			}
-			sum += Integer.parseInt(sb.toString());
-			System.out.println(sum);
 		}
 
-		answer = sum;
-	}
-
-	private char change(char x){
-		if(x == 'A') return '9';
-		if(x == 'B') return '8';
-		if(x == 'C') return '7';
-		if(x == 'D') return '6';
-		if(x == 'E') return '5';
-		if(x == 'F') return '4';
-		if(x == 'G') return '3';
-		if(x == 'H') return '2';
-		if(x == 'I') return '1';
-		return '0';
-	}
-
-	private void combination() {
-		if(index == n){
-			calculate();
-			return;
-		}
-
-		for (int i = start; i < n; i++) {
-			result[index] = strings[i];
-			start = i + 1;
-			index++;
-			combination();
+		Arrays.sort(alpha);
+		int index = 9;
+		int sum =0;
+		for(int i=alpha.length-1; i>=0; i--) {
+			if (alpha[i] == 0) {
+				break;
+			}
+			sum += alpha[i] * index;
 			index--;
 		}
-
-	}
-
-	private void print() {
-		for (int i = 0; i < n; i++) {
-			System.out.print(result[i] + " ");
-		}
-		System.out.println();
+		return sum;
 	}
 
 	@Test
