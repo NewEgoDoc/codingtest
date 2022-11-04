1d2beff1f4779a5df553312cd8eb82076b03c0b6
Juhan <juhan211@naver.com>
2022-11-04 16:57:05 +0900

    백조의 호수 풀기!

---
+++ src/main/java/coding/test/baekjoon/bfs/boj3197.java
@@ -0,0 +1,44 @@
+package coding.test.baekjoon.bfs;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class boj3197 {
+
+	int solution(int N, int M, char[][] spaces){
+
+		return 0;
+	}
+
+	@Test
+	void test(){
+		Assertions.assertEquals(solution(10,2,new char[][]{
+			{'.','L'},
+			{'.','.'},
+			{'X','X'},
+			{'X','X'},
+			{'X','X'},
+			{'X','X'},
+			{'X','X'},
+			{'X','X'},
+			{'.','.'},
+			{'.','L'}
+		}),3);
+		Assertions.assertEquals(solution(4,11,new char[][]{
+			{'.','.','X','X','X','.','.','.','X','.','.'},
+			{'.','X','.','X','X','X','.','.','.','L','.'},
+			{'.','.','.','.','X','X','X','.','.','X','.'},
+			{'X','.','L','.','.','X','X','X','.','.','.'}
+		}),3);
+		Assertions.assertEquals(solution(8,17,new char[][]{
+			{'.','.','.','X','X','X','X','X','X','.','.','X','X','.','X','X','X'},
+			{'.','.','.','.','X','X','X','X','X','X','X','X','X','.','X','X','X'},
+			{'.','.','.','X','X','X','X','X','X','X','X','X','X','X','X','.','.'},
+			{'.','.','X','X','X','X','X','.','L','X','X','X','X','X','X','.','.'},
+			{'.','X','X','X','X','X','X','.','.','X','X','X','X','X','X','.','.'},
+			{'X','X','X','X','X','X','X','.','.','.','X','X','X','X','.','.','.'},
+			{'.','.','X','X','X','X','X','.','.','.','X','X','X','.','.','.','.'},
+			{'.','.','.','.','X','X','X','X','X','.','X','X','X','L','.','.','.'},
+		}),3);
+	}
+}
