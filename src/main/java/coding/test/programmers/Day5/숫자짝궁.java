6b242a8e0fea73073f5470200a20886a1411edf9
NewEgoDocs <juhan211@naver.com>
2022-11-12 02:44:01 +0900

    Day5 String 변환에서 계속 런타임에러 뜨다니... 젠장 속았다

--- src/main/java/coding/test/programmers/Day5/숫자짝궁.java
+++ src/main/java/coding/test/programmers/Day5/숫자짝궁.java
@@ -4,17 +4,43 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class 숫자짝궁 {
+
     public String solution(String X, String Y) {
-        String answer = "";
-        return answer;
+        StringBuilder sb = new StringBuilder();
+
+        int[] xCount = new int[10];
+        int[] yCount = new int[10];
+
+        for (int i = 0; i < X.length(); i++) {
+            xCount[X.charAt(i) - '0']++;
+        }
+
+        for (int i = 0; i < Y.length(); i++) {
+            yCount[Y.charAt(i) - '0']++;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            int min = Math.min(xCount[i], yCount[i]);
+            for (int j = 0; j < min; j++) {
+                sb.append(i);
+            }
+        }
+
+        if (sb.toString().equals("")) {
+            return "-1";
+        } else if (sb.toString().startsWith("0")) {
+            return "0";
+        } else {
+            return sb.toString();
+        }
     }
 
     @Test
     void test(){
-        Assertions.assertEquals(solution("100","2345"),"-1");
-        Assertions.assertEquals(solution("100",	"203045"),"0");
-        Assertions.assertEquals(solution("100",	"123450"),"10");
-        Assertions.assertEquals(solution("12321",	"42531"),"321");
+        Assertions.assertEquals(solution("100", "2345"),"-1");
+        Assertions.assertEquals(solution("100", "203045"),"0");
+        Assertions.assertEquals(solution("100", "123450"),"10");
+        Assertions.assertEquals(solution("12321", "42531"),"321");
         Assertions.assertEquals(solution("5525",	"1255"),"552");
     }
 }
