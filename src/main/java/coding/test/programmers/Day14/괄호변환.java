31f1ae761240ee5096914e2bb3f86392034f3548
NewEgoDocs <juhan211@naver.com>
2022-12-02 20:55:11 +0900

    마지막 괄호를 맞바꾸는 로직 자체는 검색으로 알게 되었는데 정말 훌륭한듯하다...

--- src/main/java/coding/test/programmers/Day14/괄호변환.java
+++ src/main/java/coding/test/programmers/Day14/괄호변환.java
@@ -3,13 +3,76 @@ package coding.test.programmers.Day14;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 public class 괄호변환 {
+    String result ;
     public String solution(String p) {
-        if(p.equals("")) return "";
+        result = "";
+        recursive(p);
+        return result;
+    }
+    public String recursive (String w){
+        String u="";
+        String v="";
 
+        // 1번 빈문자열 확인
+        if(w == ""){
+            return w;
+        }
 
+        //2번 균형잡힌 문자열로 분리하기
+        for(int i = 2; i <= w.length(); i += 2){
+            u = w.substring(0,i);
+            v = w.substring(i);
+            int left = 0;
+            int right = 0;
+            for(int j = 0; j < u.length(); j++){
 
-        return answer;
+                if(u.charAt(j) == '(') {
+                    left++;
+                    continue;
+                }
+
+                right++;
+            }
+            if(left == right){
+                break;
+            }
+        }
+
+        boolean flag = true;
+        //조건 3번: u 가 올바르면 이어붙이고 v를재귀
+        Stack<String> confirm = new Stack<>();
+        for(int i = 0; i < u.length(); i++){
+            if(u.charAt(i) == '('){
+                confirm.push("(");
+                continue;
+            }
+
+            if(confirm.empty()) {
+                flag = false;
+                break;
+            }
+
+            confirm.pop();
+        }
+        //3-1 올바른 괄호 문자열이면 v를 재귀 + u를 result 더하기
+        if(flag){
+            result += u+recursive(v);
+            return result;
+        }
+
+        //조건4: 올바른 문자열이 아닌경우
+        result+="("+recursive(v);
+        u = u.substring(1,u.length()-1);
+        u = u.replace("(","1");
+        u = u.replace(")","2");
+        u = u.replace("1",")");
+        u = u.replace("2","(");
+        result += ")" + u;
+
+        return result;
     }
 
     @Test
