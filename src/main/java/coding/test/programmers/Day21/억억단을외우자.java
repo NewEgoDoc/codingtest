291826f95d7ebb480b633cb10dfaef8f6a8c13b4
NewEgoDocs <juhan211@naver.com>
2022-12-21 20:32:50 +0900

    숫자타자대회: 제한된 숫자에 제한된 경우의 수로 인해 모두 정의해주고 각각의 경우의 수를 모두 돌아준다(재귀) 억억단을외우자: e ~ 0까지의 경우의 수를 구하고 거꾸로 돌면서 경우의 수를 구해주고 배열로 뽑으려 했는데 안됨

--- src/main/java/coding/test/programmers/Day21/억억단을외우자.java
+++ src/main/java/coding/test/programmers/Day21/억억단을외우자.java
@@ -3,11 +3,58 @@ package coding.test.programmers.Day21;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class 억억단을외우자 {
+
+    int[] countDivisors;
+    int maximumIndex;
+
     public int[] solution(int e, int[] starts) {
-        int[] answer = {};
+        int[] answer = new int[starts.length];
+        countDivisors = new int[e + 1];
+        maximumIndex = e;
+        for (int i = 1; i <= e; i++) {
+            getDivisorCount(i);
+        }
+
+        int[][] map = new int[e+1][1];
+
+        findMinimumIndex(map);
+
+        for (int i = 0; i < starts.length; i++) {
+            answer[i] = map[starts[i]][0];
+        }
 
         return answer;
+    }
+
+    // N의 소수의 개수
+    void getDivisorCount(int N) {
+        int count = 0;
+        for (int i = 1; i * i <= N; i++) {
+            if (i * i == N) count++;
+            else if (N % i == 0) count += 2;
+        }
+        countDivisors[N] = count;
+    }
+
+    void findMinimumIndex(int[][] map){
+
+        int index = 0;
+        int max = 0;
+        for (int i = maximumIndex; i >= 0; i--) {
+            System.out.println("countDivisors = " + countDivisors[i]);
+            System.out.println("index = " + index);
+            if( countDivisors[i] >= max) {
+                max = countDivisors[i];
+                index = i;
+            }
+            map[i][0] = index;
+        }
     }
 
     @Test
--- src/main/java/coding/test/programmers/Day22/숫자타자대회.java
+++ src/main/java/coding/test/programmers/Day22/숫자타자대회.java
@@ -3,16 +3,72 @@ package coding.test.programmers.Day22;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class 숫자타자대회 {
-    public int solution(String numbers) {
-        int answer = 0;
 
-        return answer;
+import java.util.*;
+
+public class 숫자타자대회 {
+    public int[][] cost = {
+        { 1, 7, 6, 7, 5, 4, 5, 3, 2, 3 },
+        { 7, 1, 2, 4, 2, 3, 5, 4, 5, 6 },
+        { 6, 2, 1, 2, 3, 2, 3, 5, 4, 5 },
+        { 7, 4, 2, 1, 5, 3, 2, 6, 5, 4 },
+        { 5, 2, 3, 5, 1, 2, 4, 2, 3, 5 },
+        { 4, 3, 2, 3, 2, 1, 2, 3, 2, 3 },
+        { 5, 5, 3, 2, 4, 2, 1, 5, 3, 2 },
+        { 3, 4, 5, 6, 2, 3, 5, 1, 2, 4 },
+        { 2, 5, 4, 5, 3, 2, 3, 2, 1, 2 },
+        { 3, 6, 5, 4, 5, 3, 2, 4, 2, 1 }
+    };
+
+    public int[][][] dp; //dp[ind][left][right]
+    public String arr;
+    public int len;
+
+    public int solve(int index, int left, int right) {
+
+        if (index == len) {
+            return 0;
+        }
+
+        //dp로 이미 거쳐왔던 연산이라면 빠져나가기
+        if (dp[index][left][right] > 0) {
+            return dp[index][left][right];
+        }
+
+        int target = arr.charAt(index) - '0';// 타겟 넘버
+        int min = Integer.MAX_VALUE;
+
+        //왼쪽손
+        if (target != right) {
+            min = Math.min(solve(index+1, target, right) + cost[left][target], min);
+        }
+
+        //오른손
+        if (target != left) {
+            min = Math.min(solve(index+1, left, target) + cost[right][target], min);
+        }
+
+        return dp[index][left][right] = min;
+    }
+
+    public int solution(String numbers) {
+        arr = numbers;
+        len = numbers.length();
+        //초기화
+        dp = new int[len][10][10];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return solve(0, 4, 6);
     }
 
     @Test
     void test(){
         Assertions.assertEquals(solution("1756"),10);
-        Assertions.assertEquals(solution("5123 "),8);
+        Assertions.assertEquals(solution("5123"),8);
     }
 }
