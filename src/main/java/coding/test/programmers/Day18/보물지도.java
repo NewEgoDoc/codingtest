6df8374e46a76976628fd913190f9d80286d81c4
NewEgoDocs <juhan211@naver.com>
2023-01-10 23:08:45 +0900

    보물지도! 착안점은! 말이되고픈 원숭이를 보고! vistied에 jumpcount를 남겨줌으로써 풀었다.

--- src/main/java/coding/test/programmers/Day18/보물지도.java
+++ src/main/java/coding/test/programmers/Day18/보물지도.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Assertions;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
-import java.util.function.DoubleToIntFunction;
 
 public class 보물지도 {
     int N;
@@ -17,99 +15,78 @@ public class 보물지도 {
     int[] jx ={2,-2,0,0};
     int[] jy ={0,0,-2,2};
     int[][] map;
-    int[][] visited;
-
-    int chance;
+    boolean[][][] visited;
 
     int min;
+
     public int solution(int n, int m, int[][] hole) {
+        min = Integer.MAX_VALUE;
         N = n;
         M = m;
-
-        chance = 1;
-
-        map = new int[M][N];
-        visited = new int[M][N];
-
-        min = Integer.MAX_VALUE;
-
-        for (int i = 0; i < hole.length; i++) {
-            int x = hole[i][0];
-            int y = hole[i][1];
-            map[M-y][x-1] = -1;
+        visited = new boolean[n][m][2];
+        map = new int[n][m];
+        for (int[] h : hole){
+            map[h[0] - 1][h[1] - 1] = 1;
         }
 
         bfs();
+
+        if (min == Integer.MAX_VALUE) {
+            return -1;
+        }
+
         return min;
     }
 
     private void bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{M-1,0});
-        visited[M-1][0] = 1;
+        q.add(new int[]{0,0,0,1});//x, y, count, jumpCount
+        visited[0][0][1] = true;
 
         while(!q.isEmpty()){
             int[] poll = q.poll();
-
             int x = poll[0];
             int y = poll[1];
-            if(x == 0 && y == N -1){
-                min = Math.min(min, visited[0][N-1] - 1);
+            int count = poll[2];
+            int jump = poll[3];
+
+            if (x == N - 1 && y == M - 1) {
+                min = Math.min(count, min);
+                break;
             }
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(isImpossible(nx, ny)) continue;
-                if(visited[nx][ny] > 0 || map[nx][ny] == -1) continue;
+                if(isImpossible(nx,ny)) continue;
+                if(visited[nx][ny][jump]) continue;
+                if(map[nx][ny] == 1) continue;
 
-                q.add(new int[]{nx, ny});
-                visited[nx][ny] = visited[x][y] + 1;
+                visited[nx][ny][jump] = true;
+                q.add(new int[]{nx, ny, count + 1, jump});//jump 안씀
             }
 
-            if(chance <= 0){
-                continue;
-            }
+            if(jump > 0){
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + jx[i];
+                    int ny = y + jy[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + jx[i];
-                int ny = y + jy[i];
+                    if(isImpossible(nx,ny)) continue;
+                    if(visited[nx][ny][jump]) continue;
+                    if(map[nx][ny] == 1) continue;
 
-                if(isImpossible(nx, ny)) continue;
-                if(visited[nx][ny] > 0 || map[nx][ny] == 1) continue;
-
-                q.add(new int[]{nx, ny});
-                visited[nx][ny] = visited[x][y] + 1;
-                chance--;
+                    visited[nx][ny][jump - 1] = true;
+                    q.add(new int[]{nx, ny, count + 1, jump - 1});//jump 안씀
+                }
             }
         }
-
-        min = Math.min(min, visited[0][N-1] - 1);
     }
 
     private boolean isImpossible(int nx, int ny) {
-        return nx < 0 || ny < 0 || nx >= M || ny >= N;
+        return nx < 0 || ny < 0 || nx >= N || ny >= M;
     }
 
-    private void printMap() {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print("\t" + map[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    private void printVisited() {
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print("\t" + visited[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
     @Test
     public void test(){
         Assertions.assertEquals(solution(4,4,new int[][]{
