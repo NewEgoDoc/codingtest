0a1b40547c4c17a5e1ecf469b2f89f95699b1bf4
NewEgoDocs <juhan211@naver.com>
2023-01-26 23:55:26 +0900

    방울 정답!

--- src/main/java/coding/test/commulearning/week3/section1/방울.java
+++ src/main/java/coding/test/commulearning/week3/section1/방울.java
@@ -4,17 +4,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 방울 {
     public int solution(int[] bell) {
         int answer = 0;
         int[] sum = new int[bell.length + 1];
-//        for (int i = 0; i < bell.length; i++) {
-//            bell[i] = bell[i] == 1? -1:1;
-//        }
+        for (int i = 0; i < bell.length; i++) {
+            bell[i] = bell[i] == 1? -1:1;
+        }
         System.out.println(Arrays.toString(bell));
         for (int i = 1; i < bell.length + 1; i++) {
             sum[i] += sum[i-1] + bell[i-1];
+        }
+
+        Map<Integer, Integer> initialIndex = new HashMap<>();
+
+        for (int i = 0; i < sum.length; i++) {
+            if(!initialIndex.containsKey(sum[i])){
+                initialIndex.put(sum[i], i);
+                continue;
+            }
+
+            answer = Math.max(answer, i - initialIndex.get(sum[i]));
         }
 
         System.out.println(Arrays.toString(sum));
