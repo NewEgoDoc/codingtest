23a51de2e9cd0509174786ed49d8fe48f3e0faac
NewEgoDocs <juhan211@naver.com>
2023-05-23 02:14:17 +0900

    불량사용자: 조합에서 순열로 모든 경우의 수를 돌면서 불량사용자에 해당하면 count up!

--- src/main/java/coding/test/programmers/Lv3/Lv3불량사용자.java
+++ src/main/java/coding/test/programmers/Lv3/Lv3불량사용자.java
@@ -6,39 +6,61 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 public class Lv3불량사용자 {
-    boolean[] visited;
-    int index;
-    int start;
-    String[] result;
-
-    int count;
-
-    Set<String[]> list;
+    HashSet<HashSet<String>> answer;
     public int solution(String[] user_id, String[] banned_id) {
-        visited = new boolean[banned_id.length];
-        index = 0;
-        start = 0;
-        result = new String[banned_id.length];
-        list = new HashSet<>();
-        dfs(user_id, banned_id);
-        for (String[] s: list){
-            System.out.println(Arrays.toString(s));
-        }
-        return 0;
+        answer = new HashSet<>();
+
+        dfs(new LinkedHashSet<>(), user_id, banned_id);
+
+        return answer.size();
     }
 
-    void dfs(String[] user_id, String[] banned_id){
-        if(index == banned_id.length){
+
+    private void dfs(HashSet<String> hs, String[] user_id, String[] banned_id) {
+        if (hs.size() == banned_id.length) {
+//            System.out.println(hs);
+            if (isSatisfied(hs, banned_id)) {
+                answer.add(new HashSet<>(hs));
+            }
             return;
         }
-        for (int i = start; i < banned_id.length; i++) {
-            start = i + 1;
-            result[index] = banned_id[i];
-            index++;
-            dfs(user_id, banned_id);
-            index--;
+
+        for (String userId : user_id) {
+            if (hs.add(userId)) {
+                dfs(hs, user_id, banned_id);
+                hs.remove(userId);
+            }
         }
     }
+
+    private boolean isSatisfied(HashSet<String> hs, String[] bannedId) {
+        int i = 0;
+
+        for (String user : hs) {
+            if (!isSameString(user, bannedId[i++])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isSameString(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < a.length(); i++) {
+            if (b.charAt(i) == '*') continue;
+
+            if (a.charAt(i) != b.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     @Test
     void test(){
         Assertions.assertEquals(solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"},
