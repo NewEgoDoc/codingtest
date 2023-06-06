f4b261781576158b5d4178da3dfcc6eb7f684d65
NewEgoDocs <juhan211@naver.com>
2023-06-06 22:45:53 +0900

    청소년 상어: 일단 모든 물고기가 움직인다는 점과 함께 상어가 먹을수 있는 최대 먹이 수를 확인하므로 DFS에 구현을 섞었다고 생각했습니다! (중간에 해당 알고리즘이 바뀔수 있기때문에 패키지 변경이 있을수 있습니다)

---
+++ src/main/java/coding/test/baekjoon/dfs/boj19236청소년상어.java
@@ -0,0 +1,10 @@
+package coding.test.baekjoon.dfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class boj19236청소년상어 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
