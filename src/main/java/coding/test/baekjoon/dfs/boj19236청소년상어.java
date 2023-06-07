44bceda89299315cad55c73c65c460ab83bbca9b
NewEgoDocs <juhan211@naver.com>
2023-06-07 23:51:27 +0900

    청소년 상어: 어려운 이유중 하나는 내가 갔던길을 돌아가는 로직이 필요!

--- src/main/java/coding/test/baekjoon/dfs/boj19236청소년상어.java
+++ src/main/java/coding/test/baekjoon/dfs/boj19236청소년상어.java
@@ -2,9 +2,37 @@ package coding.test.baekjoon.dfs;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class boj19236청소년상어 {
+
+    static int[][][] space;
+
+    static int[] dx = {-1,-1,0,1,1,1,0,-1};
+    static int[] dy = {0,-1,-1,-1,0,1,1,1};
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        space = new int[4][4][2];
+        for (int i = 0; i < 4; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 4; j++) {
+                space[i][j] = new int[]{Integer.parseInt(input[2*j]),Integer.parseInt(input[2*j+1])};
+            }
+        }
+
+//        for(int[][] sp: space){
+//            for (int[] s: sp){
+//                System.out.print(Arrays.toString(s));
+//            }
+//            System.out.println();
+//        }
+
+        dfs();
+
+    }
+
+    static void dfs(){
+
     }
 }
