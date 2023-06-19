47488aa26e4958cacb84788f78f13cd3c384aeff
NewEgoDocs <juhan211@naver.com>
2023-06-20 00:51:12 +0900

    드래곤커브 : 역시 구현은 단계들을 메서드로 빼놓고 각각을 나눠서 정복 하는것이다.

--- src/main/java/coding/test/baekjoon/samsung/boj15685드래곤커브.java
+++ src/main/java/coding/test/baekjoon/samsung/boj15685드래곤커브.java
@@ -8,13 +8,14 @@ public class boj15685드래곤커브 {
     static boolean[][] map;
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, -1, 0, 1};
+    static int ans;
 
-    static int answer;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         map = new boolean[101][101];
-        answer = 0;
+        ans = 0;
+
         int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
@@ -27,23 +28,37 @@ public class boj15685드래곤커브 {
             );
         }
 
-        checkSquare();
+        checkSquares();
 
-        System.out.println(answer);
+        System.out.println(ans);
         br.close();
     }
 
-    private static void checkSquare() {
+    private static void checkSquares() {
         for (int i = 0; i < 100; i++) {
             for (int j = 0; j < 100; j++) {
                 if (map[i][j] && map[i][j + 1] && map[i + 1][j] && map[i + 1][j + 1]) {
-                    answer++;
+                    ans++;
                 }
             }
         }
     }
 
     public static void dragonCurve(int x, int y, int d, int g) {
+        List<Integer> directions = new ArrayList<>();
+        directions.add(d);
 
+        for (int i = 1; i <= g; i++) {
+            for (int j = directions.size() - 1; j >= 0; j--) {
+                directions.add((directions.get(j) + 1) % 4);
+            }
+        }
+
+        map[x][y] = true;
+        for (Integer direction : directions) {
+            x += dx[direction];
+            y += dy[direction];
+            map[x][y] = true;
+        }
     }
 }
