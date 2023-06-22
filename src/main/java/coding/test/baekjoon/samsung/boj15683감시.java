4d3d88b3f33e20a24b7cddf9217b0d6cfafff481
NewEgoDocs <juhan211@naver.com>
2023-06-22 23:45:40 +0900

    감시 : bfs와 dfs를 섞어서 활용하기!

--- src/main/java/coding/test/baekjoon/samsung/boj15683감시.java
+++ src/main/java/coding/test/baekjoon/samsung/boj15683감시.java
@@ -7,11 +7,12 @@ import java.util.Arrays;
 
 public class boj15683감시 {
 
-    static int n;
-    static int m;
+    static int n, m;
     static int[][] office;
 
-    static int[] dx = {}
+    static int numberOfCamera;
+
+    static int[][] cameraPositions;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,21 +21,26 @@ public class boj15683감시 {
         n = Integer.parseInt(NM[0]);
         m = Integer.parseInt(NM[1]);
 
-        office = new int[n][m];
+        office = new int[8][8];
+        numberOfCamera = 0;
+        cameraPositions = new int[8][2];
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
                 office[i][j] = Integer.parseInt(input[j]);
+                if(office[i][j] > 0 && office[i][j] < 6){
+                    cameraPositions[0] = new int[]{i,j};
+                    numberOfCamera++;
+                }
             }
         }
 
 //        for(int[] o: office){
 //            System.out.println(Arrays.toString(o));
 //        }
-        for (int i = 0; i < 4; i++) {
-
-        }
-        int[][] copyOffice = copy(office);
+        // 한개의 cctv는 고정(dfs)
+        // 같은 맵들을 복사해주고
+        // cctv의 종류마다 벽이나 사무실의 끝에 도달까지 cctv 감시(단, 종류들은 뱡향마다 바라보는곳이 바뀜) - bfs로 표시
         
 
 
