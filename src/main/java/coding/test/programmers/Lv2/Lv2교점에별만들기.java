fd55382b395e600671c49a2f04797c78c0d72145
NewEgoDocs <juhan211@naver.com>
2023-06-27 23:58:01 +0900

    교점에별만들기 : 기본적인 플로우 설정!

---
+++ src/main/java/coding/test/programmers/Lv2/Lv2교점에별만들기.java
@@ -0,0 +1,18 @@
+package coding.test.programmers.Lv2;
+
+public class Lv2교점에별만들기 {
+    public String[] solution(int[][] line) {
+        // 모든 직선 쌍에 대해 반복
+        for (int i = 0; i < line.length; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                
+            }
+        }
+        //교점 좌표 구하기
+        // 정수 좌표만 저장
+        //저장된 정수들에 대해 x,y 좌표 최댓값 최솟값 구하기
+        //배열 크기 결정
+        // 2차원 배열에 표시
+        //문자열 배열로 변환 후 반환
+    }
+}
