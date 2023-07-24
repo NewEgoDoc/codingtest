5dc64b354eddf9db4db719119606f2a432bdd144
NewEgoDocs <juhan211@naver.com>
2023-07-24 23:41:33 +0900

    스도쿠 : 빈칸에 대한 포인트들을 list로 빼서 돌리고 dfs의 범주 아래에서 순환시키려면 조건 이래에 백트랙킹!

--- src/main/java/coding/test/baekjoon/dfs/boj2580스도쿠.java
+++ src/main/java/coding/test/baekjoon/dfs/boj2580스도쿠.java
@@ -1,7 +1,45 @@
 package coding.test.baekjoon.dfs;
 
-public class boj2580스도쿠 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+public class boj2580스도쿠 {
+
+    static List<int[]> emptyPoints;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] plate = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            String[] input = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                plate[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        emptyPoints = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(plate[i][j] == 0){
+                    emptyPoints.add(new int[]{i, j});
+                }
+            }
+        }
+
+        dfs(0,0);
+
+    }
+
+    private static void dfs(int i, int count){
+        if(count == emptyPoints.size()){
+
+        }
+
+        for (int i = 0; i < emptyPoints.size(); i++) {
+            if(checkHorizontal())
+        }
     }
 }
