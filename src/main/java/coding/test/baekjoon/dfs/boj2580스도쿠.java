fe4d0780e28573696f206230759333e7b315f4e1
NewEgoDocs <juhan211@naver.com>
2023-07-25 23:24:30 +0900

    스도쿠 : 백트랙킹 완료!

--- src/main/java/coding/test/baekjoon/dfs/boj2580스도쿠.java
+++ src/main/java/coding/test/baekjoon/dfs/boj2580스도쿠.java
@@ -1,17 +1,17 @@
 package coding.test.baekjoon.dfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class boj2580스도쿠 {
 
     static List<int[]> emptyPoints;
+    static int[][] plate;
+
+    static boolean isStop;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] plate = new int[9][9];
+        plate = new int[9][9];
         for (int i = 0; i < 9; i++) {
             String[] input = br.readLine().split(" ");
             for (int j = 0; j < 9; j++) {
@@ -29,17 +29,71 @@ public class boj2580스도쿠 {
             }
         }
 
-        dfs(0,0);
+        dfs(0);
+    }
+
+    private static void printPlate() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                sb.append(plate[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int index){
+        if(isStop) return;
+
+        if(index == emptyPoints.size()){
+            isStop = true;
+            printPlate();
+
+            return;
+        }
+
+        int[] now = emptyPoints.get(index);
+        for (int i = 1; i <= 9; i++) {
+            if(isImpossible(now, i)) continue;
+            plate[now[0]][now[1]] = i;
+            dfs(index + 1);
+            plate[now[0]][now[1]] = 0;
+        }
 
     }
 
-    private static void dfs(int i, int count){
-        if(count == emptyPoints.size()){
+    private static boolean isImpossible(int[] point, int i) {
+        if(isInHorizontal(point[0], i)) return true;
+        if(isInVertical(point[1], i)) return true;
+        return isInSquare(point, i);
+    }
 
-        }
+    private static boolean isInSquare(int[] point, int value) {
+        int r = 3*(point[0] / 3);
+        int c = 3*(point[1] / 3);
 
-        for (int i = 0; i < emptyPoints.size(); i++) {
-            if(checkHorizontal())
+        for (int i = r; i < r+3; i++) {
+            for (int j = c; j < c+3; j++) {
+                if(plate[i][j] == value) return true;
+            }
         }
+        return false;
+    }
+
+    private static boolean isInVertical(int column, int value) {
+        for (int i = 0; i < 9; i++) {
+            if(plate[i][column] == value) return true;
+        }
+        return false;
+    }
+
+    private static boolean isInHorizontal(int row, int value) {
+        for (int i = 0; i < 9; i++) {
+            if(plate[row][i] == value) return true;
+        }
+        return false;
     }
 }
