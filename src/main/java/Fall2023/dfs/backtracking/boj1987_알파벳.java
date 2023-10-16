4eb088cf08e6c4c212dd8351208b1c0d61b98955
NewEgoDocs <juhan211@naver.com>
2023-10-17 01:58:10 +0900

    알파벳 : DFS 의 형태에서 간단한 조건과 마스킹을 통해 정답유추

--- src/main/java/Fall2023/dfs/backtracking/boj1987_알파벳.java
+++ src/main/java/Fall2023/dfs/backtracking/boj1987_알파벳.java
@@ -3,8 +3,7 @@ package Fall2023.dfs.backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class boj1987_알파벳 {
     static int R;
@@ -17,7 +16,6 @@ public class boj1987_알파벳 {
 
     static boolean[][] visited;
     static int max;
-
     static List<Character> list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,27 +23,24 @@ public class boj1987_알파벳 {
         R = Integer.parseInt(RC[0]);
         C = Integer.parseInt(RC[1]);
         board = new char[R][C];
+        Set<Character> set = new HashSet<>();
         for (int i = 0; i < R; i++) {
             char[] inputChar = br.readLine().toCharArray();
             for (int j = 0; j < C; j++) {
                 board[i][j] = inputChar[j];
+                set.add(board[i][j]);
             }
         }
         visited = new boolean[R][C];
         max = 0;
         list = new ArrayList<>();
+        list.add(board[0][0]);
         dfs(0, 0, 0);
         System.out.println(max);
     }
 
     static void dfs(int sum, int x, int y){
-        System.out.println(sum+","+board[x][y]);
-        if(list.contains(board[x][y])){
-            if(max < sum) max = sum;
-            return;
-        }
-
-        list.add(board[x][y]);
+        max = Math.max(max, list.size());
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
@@ -53,8 +48,13 @@ public class boj1987_알파벳 {
 
             if(nx < 0 || ny < 0 || nx >= R || ny >= C) continue;
             if(visited[nx][ny]) continue;
+            if(list.contains(board[nx][ny])) continue;
 
+            visited[nx][ny] = true;
+            list.add(board[nx][ny]);
             dfs(sum + 1, nx, ny);
+            list.remove(list.size()-1);
+            visited[nx][ny] = false;
         }
     }
 }
