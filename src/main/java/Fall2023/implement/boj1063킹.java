2db1a3e201fdf94db9b9aac73c01852e9f08ddba
NewEgoDocs <juhan211@naver.com>
2023-11-07 20:35:26 +0900

    구현: 구현문제 킹

---
+++ src/main/java/Fall2023/implement/boj1063킹.java
@@ -0,0 +1,21 @@
+package Fall2023.implement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class boj1063킹 {
+    static String[] directions = {"R", "L", "B", "T", "RT", "LT", "RB", "LB"};
+    static int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};
+    static int[] dy = {0, 0, };
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] position = br.readLine().split(" ");
+
+
+
+    }
+}
