390b904e75489b9494d9153c1a45bc75e6316ca7
NewEgoDocs <juhan211@naver.com>
2023-11-10 23:30:26 +0900

    구현: 이중 for-loop을 이용해 true로 체크를 해주면서 개수 구해주면 된다!

--- src/main/java/Fall2023/implement/boj2563색종이.java
+++ src/main/java/Fall2023/implement/boj2563색종이.java
@@ -1,7 +1,27 @@
 package Fall2023.implement;
 
-public class boj2563색종이 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.StringTokenizer;
 
+public class boj2563색종이 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total = 0;
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] arr = new boolean[101][101];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if (!arr[j][k]) {
+                        arr[j][k] = true;
+                        total++;
+                    }
+                }
+            }
+        }
+        System.out.print(total);
     }
 }
