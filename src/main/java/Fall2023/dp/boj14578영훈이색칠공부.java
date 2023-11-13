44ffdfa995e9ec5f5965261e6d4e5ff019f16125
NewEgoDocs <juhan211@naver.com>
2023-11-13 22:52:30 +0900

    DP: 언제나 풀어도 어려운 dp 문제 해당 내용은 여러가지 경우의 수를 고려해서 점화식으로 뽑게 되면 풀릴듯하다

---
+++ src/main/java/Fall2023/dp/boj14578영훈이색칠공부.java
@@ -0,0 +1,13 @@
+package Fall2023.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj14578영훈이색칠공부 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
