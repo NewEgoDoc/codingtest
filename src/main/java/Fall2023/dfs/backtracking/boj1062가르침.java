550d3a8616844ac188f1053e2a940cd3887dd2aa
NewEgoDocs <juhan211@naver.com>
2023-11-22 23:37:31 +0900

    백트래킹: 조합의 개수를 파악하여 맥스 값 찾기

--- src/main/java/Fall2023/dfs/backtracking/boj1062가르침.java
+++ src/main/java/Fall2023/dfs/backtracking/boj1062가르침.java
@@ -9,6 +9,7 @@ public class boj1062가르침 {
     static int K;
 
     static String[] words;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] NK = br.readLine().split(" ");
@@ -16,10 +17,35 @@ public class boj1062가르침 {
         N = Integer.parseInt(NK[0]);
         K = Integer.parseInt(NK[1]);
 
+        if(K < 5) {
+            System.out.println(0);
+            return;
+        }
+
+        if(K == 26) {
+            System.out.println(N);
+            return;
+        }
+
         words = new String[N];
+        visited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             words[i] = br.readLine();
+        }
+
+
+    }
+    private static void combination(int index, int start){
+        if(index == K-5){
+
+        }
+        for(int i = start; i < 26; i++) {
+            if(!visited[i]){
+                visited[i]=true;
+                combination(index+1,i+1);
+                visited[i]=false;
+            }
         }
     }
 }
