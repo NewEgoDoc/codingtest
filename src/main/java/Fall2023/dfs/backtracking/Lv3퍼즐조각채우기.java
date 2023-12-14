f595074311f652f1c767ced20b5bb5f24f0d7332
NewEgoDocs <juhan211@naver.com>
2023-12-14 23:39:16 +0900

    해당 문제 확인 및 풀이 시작

---
+++ src/main/java/Fall2023/dfs/backtracking/Lv3퍼즐조각채우기.java
@@ -0,0 +1,15 @@
+package Fall2023.dfs.backtracking;
+
+import org.junit.Test;
+
+public class Lv3퍼즐조각채우기 {
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+        return answer;
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
