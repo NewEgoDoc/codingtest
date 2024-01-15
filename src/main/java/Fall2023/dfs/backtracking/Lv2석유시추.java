e662d963b69de18608e308c6e554cb276ed17540
NewEgoDocs <juhan211@naver.com>
2024-01-16 01:03:50 +0900

    예상 적중 해당 내용을 map에 결과값을 담아두고 세로줄을 set으로 적재한후에 해당 내용을 비교하며 max값 유추!

--- src/main/java/Fall2023/dfs/backtracking/Lv2석유시추.java
+++ src/main/java/Fall2023/dfs/backtracking/Lv2석유시추.java
@@ -13,42 +13,58 @@ public class Lv2석유시추 {
 
     final private int[] dx = {1,-1,0,0};
     final private int[] dy = {0,0,1,-1};
-    private int max;
+    private int count;
 
-    private boolean[][] visited;
+    private int[][] visited;
     private int sum;
-
-    private Set<Integer> sumset;
+    private int index;
+    private Map<Integer, Integer> oilMap;
 
     public int solution(int[][] land) {
         int answer = 0;
         map = land;
         r = land.length;
         c = land[0].length;
-        sumset = new HashSet<>();
-
-        for (int i = 0; i < c; i++) {
-            sum = 0;
-            visited = new boolean[r][c];
-            for (int j = 0; j < r; j++) {
-                if(map[j][i] == 1 && !visited[j][i]) {
-                    bfs(new int[]{j, i});
-                    sum += max;
+        oilMap = new HashMap<>();
+        index = 1;
+        visited = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if(visited[i][j] == 0 && map[i][j] == 1){
+                    bfs(new int[]{i, j}, index);
+                    oilMap.put(index++, count);
                 }
             }
-            sumset.add(sum);
+        }
+//        System.out.println("oilMap = " + oilMap);
+
+        int max = 0;
+        for (int i = 0; i < c; i++) {
+            int sum = 0;
+            Set<Integer> indexSet = new HashSet<>();
+            for (int j = 0; j < r; j++) {
+                if(visited[j][i] > 0){
+                    indexSet.add(visited[j][i]);
+                }
+            }
+
+            for(int index: indexSet){
+                sum += oilMap.get(index);
+            }
+
+            max= Math.max(sum, max);
         }
 
-        return Collections.max(sumset);
+        return max;
     }
 
 
-    private void bfs(int[] start){
+    private void bfs(int[] start, int index){
 
         Queue<int[]> q = new LinkedList<>();
         q.add(start);
-        visited[start[0]][start[1]] = true;
-        max = 1;
+        visited[start[0]][start[1]] = index;
+        count = 1;
 
         while(!q.isEmpty()) {
             int[] point = q.poll();
@@ -59,16 +75,78 @@ public class Lv2석유시추 {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx >= 0 && nx < r && ny >= 0 && ny < c && map[nx][ny] == 1 && !visited[nx][ny]) {
+                if (nx >= 0 && nx < r && ny >= 0 && ny < c && map[nx][ny] == 1 && visited[nx][ny] == 0) {
                     q.add(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    max++;
+                    visited[nx][ny] = index;
+                    count++;
                 }
-
             }
         }
     }
 
+
+//
+//    private int[][] map;
+//    private int r;
+//    private int c;
+//
+//    final private int[] dx = {1,-1,0,0};
+//    final private int[] dy = {0,0,1,-1};
+//    private int max;
+//
+//    private boolean[][] visited;
+//    private int sum;
+//
+//    private Set<Integer> sumset;
+//
+//    public int solution(int[][] land) {
+//        int answer = 0;
+//        map = land;
+//        r = land.length;
+//        c = land[0].length;
+//        sumset = new HashSet<>();
+//
+//        for (int i = 0; i < c; i++) {
+//            sum = 0;
+//            visited = new boolean[r][c];
+//            for (int j = 0; j < r; j++) {
+//                if(map[j][i] == 1 && !visited[j][i]) {
+//                    bfs(new int[]{j, i});
+//                    sum += max;
+//                }
+//            }
+//            sumset.add(sum);
+//        }
+//
+//        return Collections.max(sumset);
+//    }
+//
+//
+//    private void bfs(int[] start){
+//
+//        Queue<int[]> q = new LinkedList<>();
+//        q.add(start);
+//        visited[start[0]][start[1]] = true;
+//        max = 1;
+//
+//        while(!q.isEmpty()) {
+//            int[] point = q.poll();
+//            int x = point[0];
+//            int y = point[1];
+//
+//            for (int i = 0; i < 4; i++) {
+//                int nx = x + dx[i];
+//                int ny = y + dy[i];
+//
+//                if (nx >= 0 && nx < r && ny >= 0 && ny < c && map[nx][ny] == 1 && !visited[nx][ny]) {
+//                    q.add(new int[]{nx, ny});
+//                    visited[nx][ny] = true;
+//                    max++;
+//                }
+//
+//            }
+//        }
+//    }
     @Test
     public void test(){
         Assertions.assertEquals(solution(new int[][]{
