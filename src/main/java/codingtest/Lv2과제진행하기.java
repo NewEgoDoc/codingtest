273f62a8f1f032a9909c5cf66d5bf37aae169f93
NewEgoDocs <juhan211@naver.com>
2024-02-25 23:52:16 +0900

    Lv2 과제 진행하기 감을 잡지 못해서 문제부터 다시 분석해야겠다

--- src/main/java/codingtest/Lv2과제진행하기.java
+++ src/main/java/codingtest/Lv2과제진행하기.java
@@ -2,6 +2,8 @@ package codingtest;
 
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import java.util.*;
+
 
 public class Lv2과제진행하기 {
 	public String[] solution(String[][] plans) {
@@ -13,9 +15,9 @@ public class Lv2과제진행하기 {
 	public void test(){
 		Assertions.assertArrayEquals(new String[][]{{"korean", "11:40", "30"}, {"english", "12:10", "20"}, {"math", "12:30", "40"}},
 			new String[]{"korean", "english", "math"});
-		Assertions.assertEquals(new String[][]{{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}},
+		Assertions.assertArrayEquals(new String[][]{{"science", "12:40", "50"}, {"music", "12:20", "40"}, {"history", "14:00", "30"}, {"computer", "12:30", "100"}},
 			new String[]{"science", "history", "computer", "music"});
-		Assertions.assertEquals(new String[][]{{"aaa", "12:00", "20"}, {"bbb", "12:10", "30"}, {"ccc", "12:40", "10"}},
+		Assertions.assertArrayEquals(new String[][]{{"aaa", "12:00", "20"}, {"bbb", "12:10", "30"}, {"ccc", "12:40", "10"}},
 			new String[]{"bbb", "ccc", "aaa"});
 	}
 }
